f29d768f0a6e0b62b46f25a8152975763223060a
IagorSs <iagor.penaranda@uol.com.br>
2021-12-29 11:24:03 -0300

    Exercice 1 done

--- starwars.js
+++ starwars.js
@@ -3,5 +3,13 @@
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
+import { play } from './music.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+play({
+    audioUrl: 'audio/tema-sw.mp3',
+    coverImageUrl: 'imgs/logo.svg',
+    title: 'Intro',
+    artist: 'John Williams'
+}, document.body);
